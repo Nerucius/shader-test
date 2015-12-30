279f86d9d0e1399c68e3d2802c0df75fe4de7ba7
Herman Dempere <nerucius@gmail.com>
2015-12-30 16:18:57 +0100

    Initialize

---
+++ desktop/src/net/neru/game/desktop/DesktopLauncher.java
@@ -0,0 +1,21 @@
+package net.neru.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import net.neru.game.ShaderGame;
+
+public class DesktopLauncher {
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+        // LibGDX Configuration
+        config.width = 800;
+        config.height = 600;
+        config.foregroundFPS = 30;
+        config.backgroundFPS = -1;
+        config.useGL30 = true;
+
+        new LwjglApplication(new ShaderGame(), config);
+    }
+}
